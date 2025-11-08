cb2dced1149e1c9e7a08b55498a32c75edb7a209
Bareera-Amjad <bareeraamjad04@gmail.com>
2025-11-08 02:23:30 -0800

    Add files via upload
    first upload

---
+++ first upload.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+struct Song {
+    string title;
+    string artist;
+    bool favorite;
+};
+
+class RecentlyPlayed {
+private:
+    Song songs[5];
+    int count;
+
+public:
+    RecentlyPlayed() {
+        count = 0;
+    }
+
+    void addRecent(string title, string artist) {
+        int limit = (count < 5) ? count : 4;
+        for (int i = limit; i > 0; i--) {
+            songs[i] = songs[i-1];
+        }
+        songs[0].title = title;
+        songs[0].artist = artist;
+        songs[0].favorite = false;
+        if (count < 5) count++;
+    }
+
+    void showRecent() {
+        if (count == 0) {
+            cout << "No recently played songs yet!\n";
+            return;
+        }
+        cout << "Recently Played Songs:\n";
+        for (int i = 0; i < count; i++) {
+            cout << i+1 << ") " << songs[i].title << " - " << songs[i].artist << endl;
+        }
+    }
+};
+
